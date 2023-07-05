8f7028a1e9ab440b380fd6bf98a119d0fa844096
Pradeep, Joseph <Joseph.Pradeep@astrazeneca.com>
2023-07-05 20:12:15 +0530

    added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+import { BrowserRouter} from 'react-router-dom'
+
+
 
 export default () => {
 
-    return <>
-    <h1>Hi There!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
-    <hr />
-    <MarketingApp />
-    </>
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    ) 
 };
