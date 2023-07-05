eaca8d4f749e6256ef86eb36fb3ccfb8646fdbf1
Pradeep, Joseph <Joseph.Pradeep@astrazeneca.com>
2023-07-05 21:39:05 +0530

    randamize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 import { BrowserRouter} from 'react-router-dom'
 
-
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
 
     return (
+        <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
             <div>
                 <Header />
                 <MarketingApp />
             </div>
         </BrowserRouter>
+        </StylesProvider>
     ) 
 };
