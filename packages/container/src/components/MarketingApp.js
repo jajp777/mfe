0cd78a4b7ea31ec2a35250e2bfc654d3620763ea
Pradeep, Joseph <Joseph.Pradeep@astrazeneca.com>
2023-07-05 19:50:06 +0530

    Added marketing deploymentv3

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -4,11 +4,11 @@ import React, {useRef, useEffect} from 'react';
 
 export default () => {
     const ref = useRef(null);
-    console.log('[ref1] MarketingApp', ref.current);
+    // console.log('[ref1] MarketingApp', ref.current);
 
     useEffect(() => {
         mount(ref.current);
-        console.log('[ref] MarketingApp', ref.current);
+        // console.log('[ref] MarketingApp', ref.current);
     })
     return <div ref={ref} />;
 };
